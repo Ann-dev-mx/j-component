41e3b20942f99db140e70548b580acababe01718
Akhil Xavier <axavier@mersive.com>
2020-10-25 12:16:54 +0530

    second commit

--- server.py
+++ server.py
@@ -1,16 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 import psycopg2
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
-print(__name__)
 
-@app.route('/')
-def hello_world():
-   return render_template('./index.html')
+con = psycopg2.connect(
+    host="localhost",
+    port="5432",
+    database="j-com",
+    user="postgres",
+    password="admin"
+    )
 
 
-@app.route('/register')
-def blog():
-   return render_template('./register.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+   return render_template('index.html')
+
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        Username = request.form['Username']
+        email = request.form['email']
+        password = request.form['password']
+        cur = con.cursor()
+        cur.execute("insert into register (username,email,password) values (%s,%s,%s)", (Username, email, password))
+        con.commit()
+        cur.close()
+
+        return redirect(url_for('register'))
+    else:
+        return render_template("register.html")
+#con.close( )
+#Last line
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
